23bc30e400b885af33e9fdd4961c4bf834b46ad2
smi03035 <smi03035@umn.edu>
2026-03-14 16:37:56 -0500

    Nuked all the vite stuff

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,16 @@ function App() {
     <>
       <div>
         <h1>Nils Smith </h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
       </div>
-      <h1>Vite + React</h1>
+  
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> Nils and save to test HMR
-        </p>
+       
       </div>
-      <p className="read-the-docs">
-        Nils was here. He he he he
-      </p>
+     
     </>
   )
 }
